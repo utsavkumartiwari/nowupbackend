909f1128268079361f7c13a3f52a01b4c65dfad7
Utsav Kumar Tiwari <92303874+utsavkumartiwari@users.noreply.github.com>
2026-04-07 22:09:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-co
 const path = require('path');
 
 const authRoutes = require('./routes/authRoutes');

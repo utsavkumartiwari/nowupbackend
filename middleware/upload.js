3c105fbec1b5a04c3506343dd3adb5c20236611c
Utsav Kumar Tiwari <92303874+utsavkumartiwari@users.noreply.github.com>
2026-04-08 10:29:38 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,1 +1,15 @@
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('../config/cloudinary');
 
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'news',
+    allowed_formats: ['jpg', 'png', 'jpeg']
+  }
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
